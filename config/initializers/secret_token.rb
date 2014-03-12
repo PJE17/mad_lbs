98966d308a4ae0e7416b7c113830f06cdd6d9b60
Your Name <you@example.com>
2014-03-12 18:51:32 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MadLbs::Application.config.secret_key_base = '9d981ce73384de7b40aff4afe5bd8e8eee8338a4fed50af8fd80ea52b660f764a8ccba27b1e0260172ff9b7bc6f28af9131407906e82a3a009fa832890b50f2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MadLbs::Application.config.session_store :cookie_store, key: '_mad_lbs_session'
